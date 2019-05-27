4646df0545cf68110093e3aa3dee2d5e8b4ba32e
gicabral <38368320+gicabral@users.noreply.github.com>
2019-05-27 00:04:39 -0300

    Tentando fazer o scroll da primeira pagina

--- app/src/main/java/br/edu/insper/al/mayraprl/bro/UsersSettings.java
+++ app/src/main/java/br/edu/insper/al/mayraprl/bro/UsersSettings.java
@@ -1,63 +1,31 @@
 package br.edu.insper.al.mayraprl.bro;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+        import android.support.v7.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
 
 public class UsersSettings extends AppCompatActivity {
-
-    private TextView textEmail, textID;
-    private Button btnLogOut;
-    private FirebaseAuth auth;
-    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_users_settings);
-        initializeComponents();
-        clickEvents();
+        setContentView(R.layout.activity_main);
 
-    }
-
-    private void clickEvents(){
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
+        Button btnShowOverlay = findViewById(R.id.show_overlay);
+        btnShowOverlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Conection.logOut();
-                finish();
+                launchOverlayService();
+
             }
         });
     }
 
-    private void initializeComponents(){
-        textEmail = (TextView) findViewById(R.id.userEmail);
-        textID = (TextView) findViewById(R.id.userID);
-        btnLogOut = (Button) findViewById(R.id.buttonLogOut);
-
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        auth = Conection.getFirebaseAuth();
-        user = Conection.getFirebaseUser();
-        verifyUser();
-    }
-
-    private void verifyUser(){
-        if (user == null){
-            finish();
-        }else{
-            textEmail.setText("Email:"+ user.getEmail());
-            textID.setText("ID:"+ user.getUid());
-        }
+    private void launchOverlayService() {
+        Intent intent = new Intent(UsersSettings.this, OverlayService.class);
+        startService(intent);
     }
 }
