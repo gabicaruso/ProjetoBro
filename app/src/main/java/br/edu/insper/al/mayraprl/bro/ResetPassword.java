1bb105abba3f24020b0a3b2a7d179f8e2150f592
Mayra Peter <mayra11peter@gmail.com>
2019-05-27 18:07:10 -0300

    cadastro funcionando

--- app/src/main/java/br/edu/insper/al/mayraprl/bro/ResetPassword.java
+++ app/src/main/java/br/edu/insper/al/mayraprl/bro/ResetPassword.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ResetPassword extends AppCompatActivity {
 
