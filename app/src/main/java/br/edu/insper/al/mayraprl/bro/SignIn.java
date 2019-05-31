d33760649cd572698bd5b0b1f2d7e10a1cf0b5dd
borg <beatrizmkk@al.insper.edu.br>
2019-05-31 11:09:41 -0300

    tela cadastro

--- app/src/main/java/br/edu/insper/al/mayraprl/bro/SignIn.java
+++ app/src/main/java/br/edu/insper/al/mayraprl/bro/SignIn.java
@@ -1,6 +1,57 @@
 package br.edu.insper.al.mayraprl.bro;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.internal.NonNull;
+import com.google.firebase.tasks.OnCompleteListener;
+import com.google.firebase.tasks.Task;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class SignIn extends AppCompatActivity {
+    private FirebaseAuth auth;
+
+    private void createUser (String email, String password){
+        this.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignIn.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            alert("Usuário cadastrado com sucesso!");
+                            Intent goToUsersStatus = new Intent(SignIn.this, Permission.class);
+                            startActivity(goToUsersStatus);
+                            finish();
+                        }else{
+                            alert("Erro de cadastro");
+                        }
+                    }
+                });
+    }
+
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        FileInputStream serviceAccount = new FileInputStream("./serviceAccountKey.json");
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        FirebaseApp.initializeApp(options);
+
+        String customToken = FirebaseAuth.getInstance().createCustomTokenAsync(UID).get();
+        System.out.println(customToken);
+    }
+
+}
+
+/*
+package br.edu.insper.al.mayraprl.bro;
+
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,14 +62,25 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 
 public class SignIn extends AppCompatActivity {
 
     private EditText editEmail, editPassword;
     private Button btnBack, btnSignIn;
-    private FirebaseAuth auth;
+    private com.google.firebase.auth.FirebaseAuth auth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +88,7 @@ public class SignIn extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
         initializeComponents();
         clickEvents();
+
     }
 
     private void initializeComponents(){
@@ -54,7 +117,7 @@ public class SignIn extends AppCompatActivity {
     }
 
     private void createUser (String email, String password){
-        auth.createUserWithEmailAndPassword(email, password)
+        this.auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(SignIn.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -70,6 +133,21 @@ public class SignIn extends AppCompatActivity {
                 });
     }
 
+    private static final String UID = "some_uid";
+
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        FileInputStream serviceAccount = new FileInputStream("./serviceAccountKey.json");
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        FirebaseApp.initializeApp(options);
+
+        String customToken = FirebaseAuth.getInstance().createCustomTokenAsync(UID).get();
+        System.out.println(customToken);
+    }
+
+
+
     @Override
     protected void onStart(){
         super.onStart();
@@ -81,3 +159,4 @@ public class SignIn extends AppCompatActivity {
         Toast.makeText(SignIn.this, msg,Toast.LENGTH_LONG).show();
     }
 }
+*/
